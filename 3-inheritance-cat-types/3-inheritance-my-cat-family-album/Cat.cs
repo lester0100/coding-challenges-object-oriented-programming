5791d0c71097a61b02311079b8214c1ad20402ce
lester0100 <lester46144@gmail.com>
2025-12-20 18:02:07 +0800

    Add inheritace practice project

---
+++ 3-inheritance-cat-types/3-inheritance-my-cat-family-album/Cat.cs
@@ -0,0 +1,82 @@
+﻿using _3_inheritance_my_cat_family_album;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_inheritance_my_cat_family_album
+{
+    class Cat
+    {
+        public string name { get; set; }
+        public string color { get; set; }
+        public int age { get; set; }
+
+        public Cat(string name, string color, int age)
+        {
+            this.name = name;
+            this.color = color;
+            this.age = age;
+        }
+
+        public virtual void CatProfile()
+        {
+            Console.WriteLine($"Name:   {name}\nColor:  {color}\nAge:   {age}");
+        }
+    }
+
+    class HouseCat : Cat
+    {
+        public string breed { get; set; }
+
+        public HouseCat(string name, string color, int age, string breed) : base(name, color, age)
+        {
+            this.breed = breed;
+        }
+
+        public void HouseCatProfile()
+        {
+            base.CatProfile();
+            Console.WriteLine($"Breed:  {breed}");
+        }
+    }
+    
+    class StrayCat : HouseCat
+    {
+        public string territory { get; set; }
+
+        public StrayCat(string name, string color, int age, string breed, string territory) : base(name, color, age, breed)
+        {
+            this.territory = territory;
+        }
+
+        public void Fight()
+        {
+            Console.WriteLine($"{name} is fighting.");
+        }
+
+        public void StrayCatProfile()
+        {
+            base.HouseCatProfile();
+            Console.WriteLine($"Territory:  {territory}");
+        }
+    }
+
+    class RescueCat : HouseCat
+    {
+        public string shelter { get; set; }
+        public bool isVaccinated { get; set; }
+
+        public RescueCat(string name, string color, int age, string breed, string shelter, bool isVaccinated) : base(name, color, age, breed)
+        {
+            this.shelter = shelter;
+            this.isVaccinated = isVaccinated;
+        }
+
+        public void RescueCatProfile()
+        {
+            base.HouseCatProfile();
+            Console.Write($"Shelter:    {shelter}\nVaccine status:  ");
+            Console.WriteLine(isVaccinated ? "Vaccinated" : "Not vaccinated");
+        }
+    }
+}
---
+++ 3-inheritance-cat-types/3-inheritance-my-cat-family-album/Program.cs
@@ -0,0 +1,23 @@
+﻿using _3_inheritance_my_cat_family_album;
+
+HouseCat cat1 = new HouseCat(name: "Orange", color: "Orange", age: 5, breed: "No breed");
+HouseCat cat2 = new HouseCat(name: "Gusion", color: "Gray", age: 1, breed: "Persian");
+StrayCat cat3 = new StrayCat(name: "Jawhead", color: "Panda", age: 3, breed: "No breed", territory: "First street");
+StrayCat cat4 = new StrayCat(name: "Oreo", color: "Dark brown", age: 6, breed: "Siamese", territory: "Third street");
+RescueCat cat5 = new RescueCat(name: "Coffee", color: "Dark brown", age: 4, breed: "Siamese", shelter:  "Animal Society", isVaccinated: true);
+RescueCat cat6 = new RescueCat(name: "Mythic", color: "Panda", age: 7, breed: "No breed", shelter: "Cat Foundation", isVaccinated: false);
+
+cat1.HouseCatProfile();
+Console.WriteLine();
+cat2.HouseCatProfile();
+Console.WriteLine();
+
+cat3.StrayCatProfile();
+cat3.Fight();
+Console.WriteLine();
+cat4.StrayCatProfile();
+Console.WriteLine();
+
+cat5.RescueCatProfile();
+Console.WriteLine();
+cat6.RescueCatProfile();
